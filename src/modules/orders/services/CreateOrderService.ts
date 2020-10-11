087fbfd2d4ed30f5e95b8c86be0244aa7ce2b3c4
Anderson Viana <andiana@hotmnai.com>
2020-10-11 15:40:56 -0300

    implementação dos services

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,80 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    if (!customer_id) {
+      throw new AppError('invalid customer_id');
+    }
+    if (!products) {
+      throw new AppError('invalid products');
+    }
+
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const ids = products.map(product => {
+      return { id: product.id };
+    });
+
+    const productsList = await this.productsRepository.findAllById(ids);
+
+    if (productsList.length < 1) {
+      throw new AppError('Products not found');
+    }
+
+    // criar order products
+    const orderProductsList = products.map(item => {
+      const indexProductList = productsList.findIndex(
+        product => product.id === item.id,
+      );
+      if (item.quantity > productsList[indexProductList].quantity) {
+        throw new AppError(
+          `Insufficient product quantity:
+          ${productsList[indexProductList].name}
+          . Only
+          ${productsList[indexProductList].quantity}
+           available`,
+        );
+      }
+      return {
+        product_id: item.id,
+        price: productsList[indexProductList].price,
+        quantity: item.quantity,
+      };
+    });
+
+    // criar order
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProductsList,
+    });
+
+    // atualizar quantidades
+    const updateList = products.map(item => {
+      const productListIndex = productsList.findIndex(
+        product => product.id === item.id,
+      );
+      return {
+        id: item.id,
+        quantity: productsList[productListIndex].quantity - item.quantity,
+      };
+    });
+
+    await this.productsRepository.updateQuantity(updateList);
+
+    // retornar order
+    return order;
   }
 }
 
